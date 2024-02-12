513dfc58ce947bdeaf42e2f6a03fc5181d348742
gurezani <149333675+gurezani@users.noreply.github.com>
2024-02-12 01:47:48 -0300

    Sobre Enfermagem

--- index.js
+++ index.js
@@ -1,95 +1,97 @@
 const perguntas = [
-    {
-      pergunta: "O que significa 'DOM' em JavaScript?",
-      respostas: [
-        "Documento de Objeto Móvel",
-        "Modelo de Objeto de Documento",
-        "Documento de Objeto Manipulado",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual desses é um método para selecionar elementos no DOM?",
-      respostas: [
-        "getElementByID()",
-        "selectElement()",
-        "findElement()",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a função do método 'querySelector()' em JavaScript?",
-      respostas: [
-        "Selecionar um elemento pelo seu ID",
-        "Selecionar o primeiro elemento que corresponde a um seletor CSS especificado",
-        "Selecionar todos os elementos de uma determinada classe",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a maneira correta de escrever um comentário de uma única linha em JavaScript?",
-      respostas: [
-        "// Este é um comentário",
-        "<!-- Este é um comentário -->",
-        "/* Este é um comentário */",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a diferença entre '==' e '===' em JavaScript?",
-      respostas: [
-        "'==' compara os valores, '===' compara os valores e os tipos de dados",
-        "'==' compara os valores e os tipos de dados, '===' compara apenas os valores",
-        "'==' compara apenas os valores, '===' compara os tipos de dados",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a função do operador '&&' em JavaScript?",
-      respostas: [
-        "Operador de adição",
-        "Operador de concatenação",
-        "Operador lógico 'E' (AND)",
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a finalidade do método 'parseInt()' em JavaScript?",
-      respostas: [
-        "Converter um número em uma string",
-        "Converter uma string em um número inteiro",
-        "Arredondar um número para o inteiro mais próximo",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é o resultado da expressão '5 + '5' em JavaScript?",
-      respostas: [
-        "10",
-        "'55'",
-        "Erro",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "O que o método 'push()' faz em um array JavaScript?",
-      respostas: [
-        "Remove o último elemento do array",
-        "Adiciona um elemento ao início do array",
-        "Adiciona um elemento ao final do array",
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a palavra-chave usada para declarar uma variável em JavaScript?",
-      respostas: [
-        "var",
-        "let",
-        "const",
-      ],
-      correta: 0
-    },
-  ];
+  {
+    pergunta: "Qual é o principal objetivo da enfermagem?",
+    respostas: [
+      "Administrar medicamentos",
+      "Fornecer cuidados e assistência aos pacientes",
+      "Realizar cirurgias",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é um paciente geriátrico?",
+    respostas: [
+      "Um paciente com doenças cardíacas",
+      "Um paciente pediátrico",
+      "Um paciente idoso",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é o Código de Ética dos Profissionais de Enfermagem?",
+    respostas: [
+      "Um conjunto de regras para cobrar mais dos pacientes",
+      "Um guia que estabelece princípios para a conduta ética dos enfermeiros",
+      "Um livro sobre procedimentos cirúrgicos",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quais são algumas das funções do enfermeiro?",
+    respostas: [
+      "Realizar diagnósticos médicos",
+      "Planejar e coordenar a assistência ao paciente",
+      "Prescrever medicamentos",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é a Sistematização da Assistência de Enfermagem (SAE)?",
+    respostas: [
+      "Um sistema de controle de estoque de medicamentos",
+      "Um processo de trabalho do enfermeiro que organiza a assistência prestada ao paciente",
+      "Um procedimento cirúrgico específico",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quais são as competências essenciais do enfermeiro?",
+    respostas: [
+      "Apenas habilidades administrativas",
+      "Habilidades de comunicação e trabalho em equipe",
+      "Apenas habilidades técnicas",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é um diagnóstico de enfermagem?",
+    respostas: [
+      "Um diagnóstico médico feito pelo enfermeiro",
+      "Uma avaliação da qualidade do atendimento",
+      "Uma identificação de problemas de saúde reais ou potenciais",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é a importância da educação continuada para os profissionais de enfermagem?",
+    respostas: [
+      "Não é necessária, pois os enfermeiros já têm todo o conhecimento necessário",
+      "Manter-se atualizado sobre novas técnicas e práticas",
+      "É apenas uma formalidade burocrática",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é o processo de enfermagem?",
+    respostas: [
+      "Um procedimento cirúrgico",
+      "Um conjunto de tarefas administrativas",
+      "Um método sistemático de prestação de cuidados de enfermagem",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é a importância da equipe interdisciplinar na enfermagem?",
+    respostas: [
+      "Não há necessidade de trabalhar com outros profissionais",
+      "Para fornecer uma abordagem holística e completa aos cuidados ao paciente",
+      "Apenas para aumentar a carga de trabalho dos enfermeiros",
+    ],
+    correta: 1
+  },
+];
+
+
   
   const quiz = document.querySelector('#quiz')
   const template = document.querySelector('template')
